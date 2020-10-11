864c7c9de9aaa4a54c4f49db57ee4d1787496b23
khupi7 <makhup7070@gmail.com>
2020-10-11 17:11:53 +0200

    reorder 6

--- cordova/www/js/config.js
+++ cordova/www/js/config.js
@@ -1,6 +1,6 @@
-var krms_config ={		
-	'ApiUrl':"http://www.eatmo.co.za/mobileapp/api",				
-	'DialogDefaultTitle':"EatMo",	
-	'APIHasKey':"1f1f127b1af246e10bb16ab14711b0ec",
+var krms_config ={						
+	'ApiUrl':"http://www.eatmo.co.za/mobileappv2/api",    
+	'AppTitle':"EatMo",
+	'ApiKey' : '1f1f127b1af246e10bb16ab14711b0ec',	
 	'debug': false
 };
